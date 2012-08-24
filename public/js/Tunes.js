7e33ba123efcb3f07c71799a869340394f708bb6
Ryan Lewis <s.crispinian@gmail.com>
2012-08-24 10:34:05 -0500

    Setting up album model and view

--- public/js/Tunes.js
+++ public/js/Tunes.js
@@ -1,3 +1,66 @@
 (function($) {
-
+  
+  // 'window' allows the model to be available externally
+  window.Album = Backbone.Model.extend({
+    
+    isFirstTrack: function(index) {
+      return index == 0;
+    },
+    
+    isLastTrack: function(index) {
+      return index >= this.get('tracks').length - 1;
+    },
+    
+    trackUrlAtIndex: function(index) {
+      if (this.get('tracks').length >= index) {
+        return this.get('tracks')[index].url;
+      }
+      return null;
+    }
+    
+  });
+  
+  window.AlbumView = Backbone.View.extend({
+    tagName: 'li',
+    className: 'album',
+    // id, el(overall element represented by this view)
+    
+    initialize: function() {
+      // 'bindAll' permanently associates methods with a specific object
+      _.bindAll(this, 'render');
+      
+      // change event only fires for attributes set directly with set
+      this.model.bind('change', this.render);
+      this.template = _.template($('#album-template').html());
+    },
+    
+    render: function() {
+      var renderedContent = this.template(this.model.toJSON());
+      $(this.el).html(renderedContent);
+      
+      // returns self so that multiple calls can be made to render
+      return this
+    }
+  });
+  
+  // get(key)
+  // set({ key:value })
+  // isNew()
+  // toJSON()
+  
 })(jQuery);
+
+32 31
+
+// Server-Based Controller
+//   -Act on user input: GET, POST
+//   -Render model date to templates
+//   -Send content back to browser
+
+// Backbone View
+//   -Act on user input: click, drag
+//   -Render model date to templates
+//   -Add content to current document
+
+
+// See sinonjs.org for full-featured mocks, stubs, and fake servers.
--- test-js/spec/TunesSpec.js
+++ test-js/spec/TunesSpec.js
@@ -24,7 +24,7 @@ var albumData = [{
     }]
 }];
 
-describe("Album", function () {
+describe("Album", function ()  {
 
     beforeEach(function () {
         this.album = new Album(albumData[0]);
@@ -32,6 +32,23 @@ describe("Album", function () {
 
     it("creates from data", function () {
         expect(this.album.get('tracks').length).toEqual(2);
+    });
+    
+    it("returns the URL for a track", function() {
+      expect(this.album.trackUrlAtIndex(0))
+        .toEqual('/music/Album A Track A.mp3');
+    });
+    
+    describe("first track", function() {
+      it("identifies correct first track", function() {
+          expect(this.album.isFirstTrack(0)).toBeTruthy();
+      });
+    });
+    
+    describe("last track", function() {
+      it("identifies correct last track", function() {
+          expect(this.album.isLastTrack(1)).toBeTruthy();
+      });
     });
 
 });
